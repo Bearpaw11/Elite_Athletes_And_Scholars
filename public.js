f06ebb0307250d8061528f43c501bb367b47e658
Chris Behrens <chrisbehrens@yahoo.com>
2020-02-14 22:25:34 -0700

    ade server on server.js, added html files

---
+++ public.js
@@ -0,0 +1,11 @@
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+$("#enter").on("click", function(event) {
+    event.preventDefault();
+    window.location = 'main.html';
+    
+});
+});
