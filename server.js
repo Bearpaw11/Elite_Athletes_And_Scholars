afab406d66528c6889078bf36a8df08d3afaa2b5
Chris Behrens <chrisbehrens@yahoo.com>
2020-03-02 21:30:40 -0700

    added CSS

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
 
-  app.get("/main", function(req, res) {
-    res.sendFile(path.join(__dirname, "main.html"));
-  });
+app.use(express.static(path.join(__dirname,'public')))
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
