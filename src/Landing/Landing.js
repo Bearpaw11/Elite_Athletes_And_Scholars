44658721f63073b8279d4d48f01899f49fdb7e58
Chris Behrens <chrisbehrens@yahoo.com>
2020-06-18 20:34:45 -0700

    added Landing component with button

---
+++ src/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const landing = () => {
+    return (
+        <div>
+            <button className = "enter">Click to Enter</button>
+        </div>
+    )
+}
+
+export default landing
