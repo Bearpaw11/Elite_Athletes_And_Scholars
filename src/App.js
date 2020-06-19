c5b979f520b4c83641770771f3bb760f6958bff7
Chris Behrens <chrisbehrens@yahoo.com>
2020-06-18 20:57:49 -0700

    added background image to Landing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Landing from './Landing/Landing'
 
 
 function App() {
-
+  
   return (
     
     <div>
--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,8 +1,18 @@
 import React from "react"
+import Tennis from "../images/tennis.jpeg"
 
 const landing = () => {
+    const style = {
+        marginTop: "-20px",
+        backgroundImage: `url(${Tennis})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        width: '100vw',
+        height: '100vh'
+    }
     return (
-        <div>
+        <div Tennis style ={style}>
+            <h1>Welcome to Elite Athletes and Scholors</h1>
             <button className = "enter">Click to Enter</button>
         </div>
     )
