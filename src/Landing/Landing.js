ec3cda2c3bb3aab945563c160f9674e80c5ce9d1
Chris Behrens <chrisbehrens@yahoo.com>
2020-06-18 21:31:42 -0700

    changed background image and added styling

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,19 +1,34 @@
 import React from "react"
-import Tennis from "../images/tennis.jpeg"
+import Student from "../images/student.jpg"
 
 const landing = () => {
     const style = {
         marginTop: "-20px",
-        backgroundImage: `url(${Tennis})`,
+        backgroundImage: `url(${Student})`,
         backgroundPosition: 'center',
-        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: 'black',
+        // backgroundSize: 'cover',
         width: '100vw',
         height: '100vh'
     }
+     const style1 = {
+         color: 'white',
+         paddingTop: '50px',
+         textAlign: "center",
+         fontSize: '3em'
+    }
+
+     const buttonStyle = {
+        margin: '0 auto',
+        display: 'block',
+        fontSize: '1.5em',
+        borderRadius: '10px'
+     }
     return (
-        <div Tennis style ={style}>
-            <h1>Welcome to Elite Athletes and Scholors</h1>
-            <button className = "enter">Click to Enter</button>
+        <div style ={style}>
+            <h1 style ={style1}>Welcome to Elite Athletes and Scholors</h1>
+            <button style={buttonStyle}>Click to Enter</button>
         </div>
     )
 }
