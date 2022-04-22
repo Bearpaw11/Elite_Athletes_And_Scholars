db3a1c544034f124dd9e2257a85b74a9e20473a0
Chris Behrens <chrisbehrens@yahoo.com>
2022-04-22 11:14:20 -0700

    added construction to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className={classes.background}>
       <h1 className={classes.welcome}>Welcome to Elite Athletes and Scholors</h1>
+      <h3 className={classes.construction}>This site is currently under construction</h3>
       </div>
   )
 }
